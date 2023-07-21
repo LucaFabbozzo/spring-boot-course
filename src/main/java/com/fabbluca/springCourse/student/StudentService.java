e783b6167c48b6becc0834d6a87589fe290fecab
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-07-21 18:00:16 +0200

    implementing dao layer

--- src/main/java/com/fabbluca/springCourse/student/StudentService.java
+++ src/main/java/com/fabbluca/springCourse/student/StudentService.java
@@ -5,24 +5,11 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents(){
-        return List.of(
-                new Student(
-                        "Gigio",
-                        "Rossi",
-                        LocalDate.now(),
-                        "gigiorossi@gmail.com",
-                        41
-                ),
-                new Student(
-                        "Mirko",
-                        "Verdi",
-                        LocalDate.now(),
-                        "mirkoverdi@gmail.com",
-                        25
-                )
-        );
-    }
+
+public interface StudentService {
+    Student save(Student s);
+    List <Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 }
